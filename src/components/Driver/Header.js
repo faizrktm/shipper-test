ec7fc3842bf7b8768e1fd2d5a53c54a69257827b
Full Stuck Developer <faiz@yukstay.com>
2020-07-15 22:43:28 +0700

    implement search driver name

--- src/components/Driver/Header.js
+++ src/components/Driver/Header.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { memo, useState } from 'react';
 import styled from 'styled-components';
 import breakpoint from 'styled-components-breakpoint';
 import { Add, FormSearch } from 'grommet-icons';
 
 import { Button, FloatingButton, Text, TextInput } from '../General';
+import useDebounce from '../../helper/useDebounce';
+import useNoInitialMount from '../../helper/useNoInitialMount';
+import useDispatch from '../../modules/useDispatch';
+import { searchDriver } from '../../modules/driver';
 
-const Header = () => (
-  <Container>
-    <Title>
-      <Text size="large" color="brand" weight="bold">
-        DRIVER MANAGEMENT
-      </Text>
-      <SubText size="small">Daftar Driver yang bekerja dengan Anda</SubText>
-    </Title>
-    <StyledFloatingButton icon={<Add color="white" />} />
-    <ActionWrapper>
-      <StyledTextInput
-        icon={<FormSearch color="#ff4646" size="1.5rem" />}
-        placeholder="Cari Driver"
-      />
-      <StyledButton
-        icon={<Add color="white" size="1rem" />}
-        label="TAMBAH DRIVER"
-      />
-    </ActionWrapper>
-  </Container>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+  const [searchValue, setSearchValue] = useState('');
+  const debounceStream = useDebounce(searchValue, 700);
 
-export default Header;
+  useNoInitialMount(() => {
+    dispatch(searchDriver(debounceStream));
+  }, [debounceStream]);
+
+  return (
+    <Container>
+      <Title>
+        <Text size="large" color="brand" weight="bold">
+          DRIVER MANAGEMENT
+        </Text>
+        <SubText size="small">Daftar Driver yang bekerja dengan Anda</SubText>
+      </Title>
+      <StyledFloatingButton icon={<Add color="white" />} />
+      <ActionWrapper>
+        <StyledTextInput
+          icon={<FormSearch color="#ff4646" size="1.5rem" />}
+          placeholder="Cari Driver"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <StyledButton
+          icon={<Add color="white" size="1rem" />}
+          label="TAMBAH DRIVER"
+        />
+      </ActionWrapper>
+    </Container>
+  )
+};
+
+export default memo(Header);
 
 const Container = styled.div`
   display: flex;
