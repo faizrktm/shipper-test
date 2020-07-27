903a4bcb28cf41a57fcc63f4344fd7605067a34a
Full Stuck Developer <faiz@yukstay.com>
2020-07-27 16:20:03 +0700

    install jest sixteen to support waitFor

---
+++ src/utils/test-utils.js
@@ -0,0 +1,31 @@
+import React, { useMemo } from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+
+const TestWrapper = ({ children }) => {
+  const mocks = useMemo(() => {
+    try {
+      const {
+        props: { apolloMockForTestOnly },
+      } = React.Children.only(children);
+      return apolloMockForTestOnly || [];
+    } catch (error) {
+      return [];
+    }
+  }, []);
+  return (
+    <MockedProvider mocks={mocks}>
+      <BrowserRouter>
+        <Switch>{children}</Switch>
+      </BrowserRouter>
+    </MockedProvider>
+  );
+};
+
+const customRender = (ui, options) => {
+  return render(ui, { wrapper: TestWrapper, ...options });
+};
+
+export * from '@testing-library/react';
+export { customRender as render };
